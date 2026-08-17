8e84bb3d3acde1cdfe164f6e1c6a73cd0b99dc17
00660 <00660@users.noreply.github.com>
2026-08-17 15:30:52 +0000

    Set fan speed from ring zones

--- app/src/main/java/com/wight/hawidget/HaFanWidgetProvider.java
+++ app/src/main/java/com/wight/hawidget/HaFanWidgetProvider.java
@@ -19,9 +19,38 @@ public final class HaFanWidgetProvider extends AppWidgetProvider {
     private static final String ACTION_TOGGLE = "com.wight.hawidget.FAN_TOGGLE";
     private static final String ACTION_NATURAL = "com.wight.hawidget.FAN_NATURAL";
     private static final String ACTION_SLEEP = "com.wight.hawidget.FAN_SLEEP";
+    private static final String ACTION_SET_SPEED = "com.wight.hawidget.FAN_SET_SPEED";
+    private static final String EXTRA_SPEED = "speed";
     private static final String NATURAL_PRESET = "自然风";
     private static final String SLEEP_PRESET = "睡眠风";
     private static final ExecutorService NETWORK_EXECUTOR = Executors.newSingleThreadExecutor();
+    private static final int[] SPEED_ZONE_IDS = {
+            R.id.fan_speed_001, R.id.fan_speed_002, R.id.fan_speed_003, R.id.fan_speed_004,
+            R.id.fan_speed_005, R.id.fan_speed_006, R.id.fan_speed_007, R.id.fan_speed_008,
+            R.id.fan_speed_009, R.id.fan_speed_010, R.id.fan_speed_011, R.id.fan_speed_012,
+            R.id.fan_speed_013, R.id.fan_speed_014, R.id.fan_speed_015, R.id.fan_speed_016,
+            R.id.fan_speed_017, R.id.fan_speed_018, R.id.fan_speed_019, R.id.fan_speed_020,
+            R.id.fan_speed_021, R.id.fan_speed_022, R.id.fan_speed_023, R.id.fan_speed_024,
+            R.id.fan_speed_025, R.id.fan_speed_026, R.id.fan_speed_027, R.id.fan_speed_028,
+            R.id.fan_speed_029, R.id.fan_speed_030, R.id.fan_speed_031, R.id.fan_speed_032,
+            R.id.fan_speed_033, R.id.fan_speed_034, R.id.fan_speed_035, R.id.fan_speed_036,
+            R.id.fan_speed_037, R.id.fan_speed_038, R.id.fan_speed_039, R.id.fan_speed_040,
+            R.id.fan_speed_041, R.id.fan_speed_042, R.id.fan_speed_043, R.id.fan_speed_044,
+            R.id.fan_speed_045, R.id.fan_speed_046, R.id.fan_speed_047, R.id.fan_speed_048,
+            R.id.fan_speed_049, R.id.fan_speed_050, R.id.fan_speed_051, R.id.fan_speed_052,
+            R.id.fan_speed_053, R.id.fan_speed_054, R.id.fan_speed_055, R.id.fan_speed_056,
+            R.id.fan_speed_057, R.id.fan_speed_058, R.id.fan_speed_059, R.id.fan_speed_060,
+            R.id.fan_speed_061, R.id.fan_speed_062, R.id.fan_speed_063, R.id.fan_speed_064,
+            R.id.fan_speed_065, R.id.fan_speed_066, R.id.fan_speed_067, R.id.fan_speed_068,
+            R.id.fan_speed_069, R.id.fan_speed_070, R.id.fan_speed_071, R.id.fan_speed_072,
+            R.id.fan_speed_073, R.id.fan_speed_074, R.id.fan_speed_075, R.id.fan_speed_076,
+            R.id.fan_speed_077, R.id.fan_speed_078, R.id.fan_speed_079, R.id.fan_speed_080,
+            R.id.fan_speed_081, R.id.fan_speed_082, R.id.fan_speed_083, R.id.fan_speed_084,
+            R.id.fan_speed_085, R.id.fan_speed_086, R.id.fan_speed_087, R.id.fan_speed_088,
+            R.id.fan_speed_089, R.id.fan_speed_090, R.id.fan_speed_091, R.id.fan_speed_092,
+            R.id.fan_speed_093, R.id.fan_speed_094, R.id.fan_speed_095, R.id.fan_speed_096,
+            R.id.fan_speed_097, R.id.fan_speed_098, R.id.fan_speed_099, R.id.fan_speed_100
+    };
 
     public static void requestRefresh(Context context) {
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
@@ -45,7 +74,7 @@ public final class HaFanWidgetProvider extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (isControlAction(action)) {
-            runCommand(context, action);
+            runCommand(context, action, intent.getIntExtra(EXTRA_SPEED, -1));
             return;
         }
         super.onReceive(context, intent);
@@ -54,10 +83,11 @@ public final class HaFanWidgetProvider extends AppWidgetProvider {
     private boolean isControlAction(String action) {
         return ACTION_TOGGLE.equals(action)
                 || ACTION_NATURAL.equals(action)
-                || ACTION_SLEEP.equals(action);
+                || ACTION_SLEEP.equals(action)
+                || ACTION_SET_SPEED.equals(action);
     }
 
-    private void runCommand(Context context, String action) {
+    private void runCommand(Context context, String action, int speed) {
         Context applicationContext = context.getApplicationContext();
         if (ACTION_NATURAL.equals(action) || ACTION_SLEEP.equals(action)) {
             WidgetPreferences.saveSelectedPreset(applicationContext, presetFor(action));
@@ -68,6 +98,9 @@ public final class HaFanWidgetProvider extends AppWidgetProvider {
             try {
                 if (ACTION_TOGGLE.equals(action)) {
                     EspHomeClient.toggleFan();
+                } else if (ACTION_SET_SPEED.equals(action) && speed >= 1 && speed <= 100) {
+                    EspHomeClient.setFanPercentage(speed);
+                    WidgetPreferences.saveBaseSpeed(applicationContext, speed);
                 } else {
                     EspHomeClient.setFanPreset(presetFor(action));
                 }
@@ -186,7 +219,12 @@ public final class HaFanWidgetProvider extends AppWidgetProvider {
             views.setOnClickPendingIntent(R.id.fan_power_tile, commandIntent(context, ACTION_TOGGLE, id));
             views.setOnClickPendingIntent(R.id.fan_natural_button, commandIntent(context, ACTION_NATURAL, id));
             views.setOnClickPendingIntent(R.id.fan_sleep_button, commandIntent(context, ACTION_SLEEP, id));
-            views.setOnClickPendingIntent(R.id.fan_power_button, speedIntent(context, id));
+            for (int speed = 1; speed <= 100; speed++) {
+                views.setOnClickPendingIntent(
+                        SPEED_ZONE_IDS[speed - 1],
+                        speedCommandIntent(context, id, speed)
+                );
+            }
             manager.updateAppWidget(id, views);
         }
     }
@@ -228,12 +266,16 @@ public final class HaFanWidgetProvider extends AppWidgetProvider {
         );
     }
 
-    private PendingIntent speedIntent(Context context, int appWidgetId) {
-        Intent intent = new Intent(context, SpeedActivity.class)
-                .setData(Uri.parse("hawidget://" + context.getPackageName() + "/" + appWidgetId + "/speed"));
-        return PendingIntent.getActivity(
+    private PendingIntent speedCommandIntent(Context context, int appWidgetId, int speed) {
+        Intent intent = new Intent(context, HaFanWidgetProvider.class)
+                .setAction(ACTION_SET_SPEED)
+                .setData(Uri.parse(
+                        "hawidget://" + context.getPackageName() + "/" + appWidgetId + "/speed/" + speed
+                ))
+                .putExtra(EXTRA_SPEED, speed);
+        return PendingIntent.getBroadcast(
                 context,
-                appWidgetId + 100,
+                appWidgetId * 1000 + speed,
                 intent,
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
         );
