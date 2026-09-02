fda4790468bd4f81f445916fcaa7412c47d727c1
“00660” <avu888@88.com>
2026-09-02 20:40:02 +0800

    修复环境扫描编译导入

--- app/src/main/java/com/wight/hawidget/EspHomeWebClient.java
+++ app/src/main/java/com/wight/hawidget/EspHomeWebClient.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ConcurrentHashMap;
